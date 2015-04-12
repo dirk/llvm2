5303eae4494ff770a5a8ab87984815031fee9eb8
Dirk Gadsden <dirk@esherido.com>
2015-04-12 09:53:22 -0400

    Add basic tests

--- README.md
+++ README.md
@@ -12,13 +12,22 @@ npm install llvm2
 
 llvm2 currently requires the Homebrew version of LLVM 3.5 being installed and linked. You will need to use the `--force` option to force Homebrew to link it, since linking this LLVM can interfere with the default system (ie. Apple's) development tools.
 
-```bash
+```sh
 $ brew install llvm
 ...
 $ brew ls --versions llvm
 llvm 3.5.1
 $ brew link -f llvm
 Linking /usr/local/Cellar/llvm/3.5.1... 195 symlinks created
+```
+
+## Testing
+
+The basic test suite can be run using [mocha](http://mochajs.org/).
+
+```sh
+$ npm install -g mocha@latest
+$ mocha test
 ```
 
 ## License
---
+++ test/test-library.js
@@ -0,0 +1,38 @@
+
+var LLVM    = require('../index'),
+    Library = LLVM.Library,
+    expect  = require('expect.js')
+
+describe('Type', function () {
+  var addressSpace = 0
+
+  var int64Type = null,
+      int64PtrType = null
+  it('should be created', function () {
+    int64Type = Library.LLVMInt64Type()
+    expect(int64Type).to.be.a(Buffer)
+    int64PtrType = Library.LLVMPointerType(int64Type, addressSpace)
+    expect(int64PtrType).to.be.a(Buffer)
+  })
+  it('should be dumped', function () {
+    var str = Library.LLVMPrintTypeToString(int64Type)
+    expect(str).to.eql('i64')
+    str = Library.LLVMPrintTypeToString(int64PtrType)
+    expect(str).to.eql('i64*')
+  })
+
+  it('should fail to create a pointer without an address space', function () {
+    expect(function () {
+      Library.LLVMPointerType(Library.LLVMInt8Type())
+    }).to.throwException(/Expected 2 arguments, got 1/)
+  })
+})
+
+describe('Context', function () {
+  it('should get a global context', function () {
+    var ctx = Library.LLVMGetGlobalContext()
+    expect(ctx).to.be.a(Buffer)
+    expect(ctx.isNull()).to.be(false)
+  })
+})
+
